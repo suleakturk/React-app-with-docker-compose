122bf229a9a7b261bd801a7fcaff441a0c810cdc
unknown <sule.akturk@d-teknoloji.com.tr>
2023-04-16 02:14:53 +0300

    feat: add docker-compose file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Docker Compose file!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
